72104af657f4c9cdd283977e84c95f071bee6c79
tnn4 <tnn130130@gmail.com>
2023-05-11 12:53:34 -0500

    update image name

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dockerfile="dockerfile-pg"
-image_name="tnn4/pg-gitea" && image_tag="v0.1.0"
+image_name="pg-gitea" && image_tag="v0.1.0"
 patched_pg_image="${image_name}:${image_tag}"
 
 docker build --file ${dockerfile} --tag ${patched_pg_image} .
